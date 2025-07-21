b7b1a8cede196ebb839c64140b5f109e2ddd34df
Otkr519 <romans519otkr@gmail.com>
2025-07-21 15:39:03 +0900

    Add:User/Store/Review/Likeモデルの関連付けを追加

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,2 +1,11 @@
 class Store < ApplicationRecord
+  has_many :users, through: :reviews
+
+  has_many :reviews, dependent: :destroy
+
+  has_many :likes
+  has_many :liked_users, through: :likes, source: :user
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefecture
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :reviews, dependent: :destroy
+  has_many :stores, through: :reviews
+
+  has_many :likes, dependent: :destroy
+  has_many :liked_stores, through: :likes, source: :store
 end
