61f1e6151c33835ab007e12d112a04fcfa65df64
Otkr519 <romans519otkr@gmail.com>
2025-07-27 07:39:54 +0900

    ADD:Usersテーブルにnameカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2025_07_21_055100) do
+ActiveRecord::Schema[7.0].define(version: 2025_07_26_223614) do
   create_table "likes", force: :cascade do |t|
     t.integer "user_id", null: false
     t.integer "store_id", null: false
@@ -47,6 +47,7 @@ ActiveRecord::Schema[7.0].define(version: 2025_07_21_055100) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
