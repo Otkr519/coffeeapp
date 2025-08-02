4072ee29768046e2902f81350ce5428b90bc4120
Otkr519 <romans519otkr@gmail.com>
2025-08-02 12:29:01 +0900

    ADD:UsersControllerにedit・updateアクションを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,30 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def edit
+    @user = User.find(params[:id])
+    not_match_user
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      sign_in :user, @user, bypass: true
+      redirect_to user_path(@user), notice: '更新されました。'
+    else
+      render :edit
+    end
+  end
+
+    private
+  def user_params
+    params.require(:user).permit(:name, :email, :introduce, :image, :favorite, :productionarea, :password, :password_confirmation)
+  end
+
+  def not_match_user
+    unless @user.id == current_user.id
+      redirect_to root_path , alert: '権限がありません'
+    end
+  end
+
 end
