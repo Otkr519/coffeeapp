a3d8b9b216d43f112552d307dbb7145215baa2e5
Otkr519 <romans519otkr@gmail.com>
2025-09-01 06:57:23 +0900

    Add:storesにreviewsルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,6 @@ Rails.application.routes.draw do
       get 'search'
     end
     resource :like, only: [:create, :destroy]
+    resources :reviews, only: [:new, :create, :edit, :update, :destroy]
   end
-
 end
