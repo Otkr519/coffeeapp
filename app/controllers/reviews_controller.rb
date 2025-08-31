2efc0445b9a79068bb9dce077524e72677ba6475
Otkr519 <romans519otkr@gmail.com>
2025-09-01 06:50:51 +0900

    Add:reviews_controllerにnew,createアクション追加

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,27 @@
 class ReviewsController < ApplicationController
+  before_action :set_store
+  def new
+    @review = @store.reviews.build
+  end
+
+  def create
+    @review = @store.reviews.build(review_params)
+    @review.user = current_user
+
+    if @review.save
+      redirect_to @store, notice: 'レビューを投稿しました。'
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def set_store
+    @store = Store.find(params[:store_id])
+  end
+
+  def review_params
+    params.require(:review).permit(:comment, :rating)
+  end
+
 end
