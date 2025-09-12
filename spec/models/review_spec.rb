009f9255088284e1df1c2099a8eecb8e04e5fb81
Otkr519 <romans519otkr@gmail.com>
2025-09-13 07:51:30 +0900

    Add:reviewモデルのテスト追加

---
+++ spec/models/review_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe Review, type: :model do
+  describe "バリデーションのテスト" do
+    let(:user) { create(:user) }
+    let(:store) { create(:store) }
+    let(:review) { build(:review, user: user, store: store) }
+
+    it "有効なレビューを作成できること" do
+      expect(review).to be_valid
+    end
+
+    it "userが必須であること" do
+      review.user = nil
+      expect(review).not_to be_valid
+    end
+
+    it "storeが必須であること" do
+      review.store = nil
+      expect(review).not_to be_valid
+    end
+  end
+
+  describe "アソシエーションのテスト" do
+    it "userモデルと関連付けされている" do
+      should belong_to(:user)
+    end
+    it "storeモデルと関連付けされている" do
+      should belong_to(:store)
+    end
+  end
+end
