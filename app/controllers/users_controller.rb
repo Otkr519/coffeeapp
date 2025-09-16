0a8d64beccea73c2f0d9bd32565bf2a036ebce43
Otkr519 <romans519otkr@gmail.com>
2025-09-17 06:55:32 +0900

    Add:UsersControllerのStrong Parametersに:remove_imageを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -46,7 +46,7 @@ class UsersController < ApplicationController
 
     private
   def user_params
-    params.require(:user).permit(:name, :email, :introduce, :image, :favorite, :productionarea, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :introduce, :image, :favorite, :productionarea, :password, :password_confirmation, :remove_image )
   end
 
   def not_match_user
