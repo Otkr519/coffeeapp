772963ae95b8cce2e76dd78045e8588253761085
Otkr519 <romans519otkr@gmail.com>
2025-09-26 07:04:14 +0900

    Add:rUserモデルにstoresのdependent: :destroy を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :reviews, dependent: :destroy
-  has_many :stores, through: :reviews
+  has_many :stores, dependent: :destroy
 
   has_many :likes, dependent: :destroy
   has_many :liked_stores, through: :likes, source: :store
