f58b6a797f0d803aaa37508f3d84deb2f49d61d1
Otkr519 <romans519otkr@gmail.com>
2025-09-30 06:59:19 +0900

    fix:HerokuでのS3アップロードでACLエラーになるため、aws_acl: 'public-read' を削除

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,7 +2,6 @@ CarrierWave.configure do |config|
   if Rails.env.production? && ENV['AWS_BUCKET'].present?
     config.storage = :aws
     config.aws_bucket = ENV['AWS_BUCKET']
-    config.aws_acl    = 'public-read'
     config.aws_credentials = {
       access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
       secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
